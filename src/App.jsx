1a8c3e8c470aba3a22347cacda30a011c2a1dd61
pskey35 <anonimo35371@gmail.com>
2024-12-08 10:37:36 -0500

    first commit- login.jsx worked

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "../public/styles/index.scss"
+import Home from "./routes/home/home.jsx"
+import Login from "./routes/login/login.jsx"
+import { useState, useEffect } from "react"
+
+
+
+export default function App() {
+
+
+
+
+  useEffect(() => {
+    const darkTheme = localStorage.getItem("dark")
+    const html = document.querySelector("html")
+
+    if (darkTheme == "true") {
+      html.classList.add("dark")
+    } else {
+      //aqui entrara en caso no encuentra nada en localstorage o si de en si es falso el valor
+      html.classList.remove("dark")
+    }
+
+
+
+  }, [])
+
+
+
+  return (
+
+
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Home></Home>}>
+
+
+        </Route>
+
+        <Route path="/login" element={<Login></Login>}>
+
+        </Route>
+
+
+      </Routes>
+
+
+    </BrowserRouter>
+
+
+  )
+
+}
