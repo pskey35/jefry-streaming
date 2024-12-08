f58bdb08c5f2c0d688c7114384a26fd65d8beb11
pskey35 <anonimo35371@gmail.com>
2024-12-08 16:04:17 -0500

    feat: loader working in button createAccount

--- src/routes/register/register.jsx
+++ src/routes/register/register.jsx
@@ -1,24 +1,88 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 import "./register.scss"
 
 export default function Register() {
 
-    const [isLoading,setIsLoading] = useState(false)
+    const navigate = useNavigate()
 
-    const inputUserName_event = (event) =>{
-        if(event.target.value.length == 0 ){
-            event.target.style.border = "1px solid #e2e8f0"
+    const [isLoading, setIsLoading] = useState(false)
+    const [messageRegister, setMessageRegister] = useState({
+        error: false,
+        message: ""
+    })
+
+    const inputUserName_event = (event) => {
+
+        if (event.target.value.length == 0 || event.target.value.length >= 1) {
+            //   event.target.style.border = "1px solid "
+            setMessageRegister({
+                error: false,
+                message: ""
+            })
         }
     }
 
+    const clickButtonSend = async () => {
+        setIsLoading(true)
 
-    const clickButtonSend = () =>{
+        const inputUserName = document.querySelector("#inputUserName")
+        const inputPassword = document.querySelector("#inputPassword")
+
+        const inputsValue = {
+            username: inputUserName.value.trim(),
+            password: inputPassword.value.trim()
+        }
+
+
+        if (inputsValue.username.length == 0) {
+            setMessageRegister({
+                error: true,
+                message: "Put your user name"
+            })
+            setIsLoading(false)
+            return;
+        }
+
+        if (inputsValue.password.length == 0) {
+            setMessageRegister({
+                error:true,
+                message:"Put your password"
+            })
+            setIsLoading(false)
+        }
+
+
+
+
+        try {
+            const response = await fetch(`${import.meta.env.VITE_api}/register`, {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(inputsValue)
+            })
+
+            const data = await response.json()
+            console.log("register")
+            console.log(data)
+
+            setIsLoading(false)
+            navigate("/")
+
+
+        } catch (error) {
+            console.log("huubo un error")
+            console.log(error)
+        }
 
     }
 
 
-    return (
+    
 
+    return (
         <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
             <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
                 <div className="flex flex-col overflow-y-auto md:flex-row">
@@ -85,12 +149,19 @@ export default function Register() {
                             </div>
 
                             {/* You should use a button here, as the anchor is only used for the example */}
-                            <a
-                                className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-                                href="./login"
+                            <div
+                                className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple h-[38px]"
+
+                                id="buttonSend"
+                                onClick={clickButtonSend}
                             >
-                                Create account
-                            </a>
+
+                                {isLoading ?
+                                    <div className="loader"></div>
+                                    : "Create account"}
+
+
+                            </div>
 
                             <hr className="my-8" />
 
