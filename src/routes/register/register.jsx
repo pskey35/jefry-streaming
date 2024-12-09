5ff35480616e4b210031fa58d22f0b341e2153da
pskey35 <anonimo35371@gmail.com>
2024-12-09 09:06:56 -0500

    its working

--- src/routes/register/register.jsx
+++ src/routes/register/register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import "./register.scss"
 
@@ -29,8 +29,8 @@ export default function Register() {
         setIsLoading(true)
 
         setMessageRegister({
-            error:false,
-            message:""
+            error: false,
+            message: ""
         })
 
         const inputUserName = document.querySelector("#inputUserName")
@@ -46,12 +46,12 @@ export default function Register() {
             setIsLoading(false)
             inputUserName.style.border = "1px solid red";
             return;
-        }else{
+        } else {
             inputUserName.style.border = "1px solid #e2e8f0";
         }
 
 
-   
+
 
 
         const inputsValue = {
@@ -75,7 +75,7 @@ export default function Register() {
                 message: "Put your password"
             })
             setIsLoading(false)
-      
+
 
             inputPassword.style.border = "1px solid red"
             inputRepeatPassword.style.border = "1px solid red"
@@ -84,27 +84,31 @@ export default function Register() {
         }
 
 
-        if(inputRepeatPassword.value.length == 0){
+        if (inputRepeatPassword.value.length == 0) {
             inputRepeatPassword.style.border = "1px solid red"
             setMessageRegister({
-                error:true,
-                message:"Your passwords do not matched"
+                error: true,
+                message: "Your password do not matched"
             })
             setIsLoading(false)
             return;
-        }else{
+        } else {
             inputRepeatPassword.style.border = "1px solid #e2e8f0"
         }
 
-        if(inputsValue.password == inputRepeatPassword.value){
+        if (!(inputsValue.password == inputRepeatPassword.value)) {
+            inputPassword.style.border = "1px solid red"
+            inputRepeatPassword.style.border = "1px solid red"
             setMessageRegister({
-                error:true,
-                message:"Your password do not matched"
+                error: true,
+                message: "Your password do not matched"
             })
             setIsLoading(false)
             return;
         }
-      
+
+
+
 
 
 
@@ -119,6 +123,7 @@ export default function Register() {
 
             const data = await response.json()
             console.log("register")
+            localStorage.setItem("token",data.token)
             console.log(data)
 
             setIsLoading(false)
@@ -137,21 +142,28 @@ export default function Register() {
 
         const inputPassword = document.querySelector("#inputPassword")
 
-        if (inputPassword.value.trim() == event.target.value) {
-            inputPassword.style.border = "1px solid red"
-            event.target.style.border = "1px solid red"
+        if (inputPassword.value == event.target.value) {
+            inputPassword.style.border = "1px solid #e2e8f0"
+            event.target.style.border = "1px solid #e2e8f0"
             setMessageRegister({
-                error: false,
-                message: "Your password do not match"
+                error: true,
+                message: ""
             })
-            setIsLoading(false)
             return;
         }
     }
 
 
 
+    useEffect(() => {
+        const token = localStorage.getItem("token")
 
+        if (token) {
+            if (token.length > 10) {
+                navigate("/")
+            }
+        }
+    }, [])
 
 
     return (
@@ -224,7 +236,7 @@ export default function Register() {
                             {/* You should use a button here, as the anchor is only used for the example */}
                             <div
                                 className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple h-[38px]"
-
+                                style={{ cursor: "pointer" }}
                                 id="buttonSend"
                                 onClick={clickButtonSend}
                             >
