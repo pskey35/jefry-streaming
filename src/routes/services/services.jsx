421d6dd2c3320698efb6e7002fc6b4da5e27783b
pskey35 <anonimo35371@gmail.com>
2024-12-20 17:30:02 -0500

    /services cards working but just in Desktop ... in mobile look ugle

--- src/routes/services/services.jsx
+++ src/routes/services/services.jsx
@@ -30,17 +30,71 @@ function Main() {
         }
     ]
     return (
-        <div>
+        <>
+        <h2 className="text-[2em] p-8 pb-0 font-bold" >Services</h2>
+        <div className="
+        flex flex-1 flex gap-8 p-10 flex-col 
+        sm:flex-row
 
+        ">
+
+            {
+                dataSimulated && dataSimulated.map((dataUnidad, index) => {
+                    return (
+                        <div className="w-[100%] 
+                        max-w-[450px] bg-gray-100 
+                        p-4 h-auto h-[max-content]
+                        max-w-[22em]
+                        rounded-xl flex-1 flex flex-col">
+
+                            <div className="flex justify-evenly items-start mb-4">
+                                <div className="h-24 w-24">
+                                    <img src={dataUnidad.image} className="w-full h-full  object-contain"></img>
+                                </div>
+                                <div className="pt-2 pl-2">
+                                    <span style={{ fontWeight: "700" }}>{dataUnidad.name}</span>
+                                    <div className="break-all">{dataUnidad.description}</div>
+                                </div>
+                            </div>
+
+
+                            <div className="h-[50px]">
+                                <div className="w-full
+                                 bg-blue-200 p-2 rounded rounded-xl 
+                                 hover:bg-blue-400
+                                 flex justify-evenly items:center" >
+
+                                    <span 
+                                   
+                                    
+                                    style={{
+                                        color: "white",
+                                        fontSize: "16px",
+                                        display: "grid",
+                                        placeItems: "center",
+                                        fontWeight:"700"
+                                    }}>
+
+                                        {dataUnidad.type}
+                                    </span>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
+        </>
     )
 }
 
 export default function Services() {
     return (
-        <div className="flex ">
+        <div className="flex h-full">
             <Aside></Aside>
-            <div className="flex-1">
+            <div className="flex-1 flex flex-col">
                 <Header></Header>
                 <Main></Main>
             </div>
