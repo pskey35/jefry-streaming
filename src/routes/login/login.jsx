e01e0490dfbee07622d04bf0eee9f2fbed0b4f86
pskey35 <anonimo35371@gmail.com>
2025-01-06 18:23:42 -0500

    refactor(login.jsx)

--- src/routes/login/login.jsx
+++ src/routes/login/login.jsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext} from "react";
 import "./login.scss"
 import { useNavigate } from "react-router-dom"
 
+
+const LoginContext = React.createContext()
 
 const decodeJWT = (token) => {
     try {
@@ -15,26 +17,74 @@ const decodeJWT = (token) => {
 };
 
 
-function Login() {
 
-    const [isLoading, setIsLoading] = useState(false)
-    const navigate = useNavigate();
-    const [messageLogin, setMessageLogin] = useState({
-        error: false,
-        message: ""
-    })
+function InputUserName() {
 
+    const {setMessageLogin} = useContext(LoginContext)
 
-    useEffect(() => {
-        const token = localStorage.getItem("token")
-
-        if (token) {
-            if (token.length > 10) {
-                navigate("/")
-            }
+    const inputUserNameEvent = (event) => {
+        if (event.target.value.length == 0) {
+            event.target.style.border = "1px solid red"
+            setMessageLogin({
+                error: true,
+                message: "Introduce your user name"
+            })
+        } else if (event.target.value.length >= 1) {
+            event.target.style.border = "1px solid rgb(226, 232, 240)"
+            setMessageLogin({
+                error: true,
+                message: ""
+            })
         }
-    }, [])
+    }
 
+    return (
+        <label className="block text-sm">
+            <span className="text-gray-700 dark:text-gray-400">user name</span>
+            <input
+                type="email"
+                onInput={inputUserNameEvent}
+                id="login-inputUserName"
+                className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                placeholder="Jane Doe"
+            />
+        </label>
+
+    )
+}
+
+
+function InputPassword() {
+    const {setMessageLogin} = useContext(LoginContext)
+
+    const inputPasswordEvent = (event) => {
+        if (event.target.value.length >= 1) {
+            event.target.style.border = "1px solid rgb(226, 232, 240)"
+            setMessageLogin({
+                error: false
+            })
+        } else if (event.target.value.length == 0) {
+            event.target.style.border = "1px solid red"
+        }
+    }
+    return (
+        <label className="block mt-4 text-sm">
+            <span className="text-gray-700 dark:text-gray-400">Password</span>
+            <input
+                type="password"
+                onInput={inputPasswordEvent}
+                id="login-inputPassword"
+                className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                placeholder="***************"
+            />
+        </label>
+    )
+}
+
+
+function ButtonLogin() {
+    const [isLoading, setIsLoading] = useState(false)
+    const {setMessageLogin} = useContext(LoginContext)
 
     const clickButtonSend = async () => {
         const login_messageLogin = document.querySelector("#login-messageLogin")
@@ -150,123 +200,125 @@ function Login() {
     }
 
 
-    const inputUserNameEvent = (event) => {
-        if (event.target.value.length == 0) {
-            event.target.style.border = "1px solid red"
-            setMessageLogin({
-                error: true,
-                message: "Introduce your user name"
-            })
-        } else if (event.target.value.length >= 1) {
-            event.target.style.border = "1px solid rgb(226, 232, 240)"
-            setMessageLogin({
-                error: true,
-                message: ""
-            })
-        }
-    }
-
-
-    const inputPasswordEvent = (event) => {
-        if (event.target.value.length >= 1) {
-            event.target.style.border = "1px solid rgb(226, 232, 240)"
-            setMessageLogin({
-                error: false
-            })
-        } else if (event.target.value.length == 0) {
-            event.target.style.border = "1px solid red"
-        }
-    }
 
     return (
-        <div className={`flex items-center min-h-screen p-6 bg-gray-50 login-window`}>
-            <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
-                <div className="flex flex-col overflow-y-auto md:flex-row">
+        <button
+            onClick={clickButtonSend}
 
-                    <div className="h-32 md:h-auto md:w-1/2">
-                        <img
-                            aria-hidden="true"
-                            className="object-cover w-full h-full dark:hidden"
-                            src="/assets/login-office.jpeg"
-                            alt="Office"
-                        />
-                        <img
-                            aria-hidden="true"
-                            className="hidden object-cover w-full h-full dark:block"
-                            src="/assets/login-office-dark.jpeg"
-                            alt="Office"
-                        />
-                    </div>
+            id="login-buttonSend"
+            className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+        >
+            {
+                isLoading ? <div className="loader"></div>
+                    : "Log in"
+            }
+        </button>
+    )
+}
 
 
-                    <div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
-                        <div className="w-full">
-                            <h1 className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">Login</h1>
 
-                            <label className="block text-sm">
-                                <span className="text-gray-700 dark:text-gray-400">user name</span>
-                                <input
-                                    type="email"
-                                    onInput={inputUserNameEvent}
-                                    id="login-inputUserName"
-                                    className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                                    placeholder="Jane Doe"
-                                />
-                            </label>
+function LoginRight() {
 
-                            <label className="block mt-4 text-sm">
-                                <span className="text-gray-700 dark:text-gray-400">Password</span>
-                                <input
-                                    type="password"
-                                    onInput={inputPasswordEvent}
-                                    id="login-inputPassword"
-                                    className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                                    placeholder="***************"
-                                />
-                            </label>
+    const {messageLogin} = useContext(LoginContext)
+    const navigate = useNavigate();
 
-                            <button
-                                onClick={clickButtonSend}
 
-                                id="login-buttonSend"
-                                className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-                            >
-                                {
-                                    isLoading ? <div className="loader"></div>
-                                        : "Log in"
-                                }
-                            </button>
 
-                            <div style={{ color: messageLogin.error ? "red" : "green", opacity: messageLogin.error ? "1" : "0" }} id="login-messageLogin">
-                                {messageLogin.message}
-                            </div>
+    return (
+        <div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+            <div className="w-full">
+                <h1 className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">Login</h1>
 
-                            <hr className="my-8" />
+                <InputUserName></InputUserName>
+                <InputPassword></InputPassword>
+                <ButtonLogin></ButtonLogin>
 
-                            <div className="h-[90px]">
+                <div style={{ color: messageLogin.error ? "red" : "green", opacity: messageLogin.error ? "1" : "0" }} id="login-messageLogin">
+                    {messageLogin.message}
+                </div>
 
-                            </div>
-                            <p className="mt-4">
-                               
-                            </p>
-                            <p className="mt-1">
-                                <span
-                                    className="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline cursor-pointer"
-                                    style={{ color: "rgba(0, 213, 255, 0.9176470588)" }}
-                                    onClick={() => navigate("/register")}
-                                >
-                                    Create account
-                                </span>
-                            </p>
-                        </div>
+                <hr className="my-8" />
+
+                <div className="login-space  h-[90px]"></div>
+                <p className="mt-4"></p>
+
+
+                <p className="mt-1">
+                    <span
+                        className="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline cursor-pointer"
+                        style={{ color: "rgba(0, 213, 255, 0.9176470588)" }}
+                        onClick={() => navigate("/register")}
+                    >
+                        Create account
+                    </span>
+                </p>
+
+
+            </div>
+        </div>
+    )
+}
+
+
+function LoginLeft() {
+    return (
+        <div className="h-32 md:h-auto md:w-1/2">
+            <img
+                aria-hidden="true"
+                className="object-cover w-full h-full dark:hidden"
+                src="/assets/login-office.jpeg"
+                alt="Office"
+            />
+            <img
+                aria-hidden="true"
+                className="hidden object-cover w-full h-full dark:block"
+                src="/assets/login-office-dark.jpeg"
+                alt="Office"
+            />
+        </div>
+
+    )
+}
+
+
+
+
+
+function Login() {
+
+
+    const [messageLogin, setMessageLogin] = useState({
+        error: false,
+        message: ""
+    })
+
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+
+        if (token) {
+            if (token.length > 10) {
+                navigate("/")
+            }
+        }
+    }, [])
+
+
+    const value = {messageLogin,setMessageLogin}
+
+    return (
+        <LoginContext.Provider value={value}>
+            <div className={`flex items-center min-h-screen p-6 bg-gray-50 login-window`}>
+                <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
+                    <div className="flex flex-col overflow-y-auto md:flex-row">
+                        <LoginLeft></LoginLeft>
+                        <LoginRight></LoginRight>
                     </div>
                 </div>
             </div>
-        </div>
+        </LoginContext.Provider>
     );
 };
 
 
 export default Login;
-
-
